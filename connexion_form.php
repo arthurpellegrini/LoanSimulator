4f8431bfd0b0992003a231a7278f0bfdfbd6b127
Arthur Pellegrini <arthur.pellegrini@outlook.fr>
2021-12-10 16:27:08 +0100

    amélioration du connexion_form

--- connexion_form.php
+++ connexion_form.php
@@ -11,6 +11,23 @@
 </head>
 
 <body>
+    <header class="header">
+        <nav class="navbar">
+            <a href="https://www.iut-velizy-rambouillet.uvsq.fr/" class="nav-logo">
+                <img src="assets/img/logo_iut_velizy.png" height="40" alt="Logo de l'IUT de Vélizy." />
+            </a>
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">Acceuil</a>
+                </li>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+    </header>
     <?php include("php/connexion.php"); ?>
     <div class="page-connexion">
         <div class="form">
@@ -21,16 +38,39 @@
                         <input type="text" placeholder="Entrer votre identifiant" name="username" required><br>
                         <input type="password" placeholder="Entrer votre mot de passe" name="password" required><br>
                         <div class="inputs-button"><input type="submit" name="connexion" value="Connexion"></div><br>
-                        <?php if(isset($_POST['username'],$_POST['password'])){
-                                echo connect(); 
-                            }
-                            if(isset($_GET['error-message']) && $_GET['error-message']==1){
-                                echo "<p class=\"message-error\">Merci de vous authentifier</p>";
-                            } ?>
+                        <?php if (isset($_POST['username'], $_POST['password'])) {
+                            echo connect();
+                        }
+                        if (isset($_GET['error-message']) && $_GET['error-message'] == 1) {
+                            echo "<p class=\"message-error\">Merci de vous authentifier</p>";
+                        } ?>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- FOOTER -->
+	<footer>
+		<div class="social">
+			<a href="https://github.com/arthurpellegrini/loan-simulator"><img src="assets/img/github_logo.png" height="40" alt="Logo de Github." /></a>
+		</div>
+
+		<ul class="list">
+			<li>
+				<a href="#">Haut de page</a>
+			</li>
+			<li>
+				<a href="index.php">Acceuil</a>
+			</li>
+		</ul>
+
+		<p class="copyright">
+			Copyright © Nathan Delorme - Abdouramane Gadio - Léo Meyne - Arthur Pellegrini - Balthazar Puget - 2021
+		</p>
+	</footer>
 </body>
+
+<script type="text/javascript" src="script/menu.js"></script>
+
 </html>
